92d2fc224c7e6dc4ec8c74ca6cc42628e8550817
Minhyeong Lee <peterlee11@naver.com>
2025-02-28 11:04:35 +0900

    chore: update deps, configs

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,11 @@
 import { configDefaults, defineConfig } from "vitest/config"
 
+import packageJson from "./package.json"
+
 export default defineConfig({
   test: {
+    name: packageJson.name,
+    environment: "jsdom",
     include: [...configDefaults.include, "**/test/*.ts"],
   },
 })
