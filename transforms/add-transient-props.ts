fecffee34d1d79bae692c34e64374db6ba7ca2b9
Minhyeong Lee <peterlee11@naver.com>
2025-03-11 17:02:51 +0900

    refactor.

--- transforms/add-transient-props.ts
+++ transforms/add-transient-props.ts
@@ -1,4 +1,4 @@
-import type { API, FileInfo, TSTypeLiteral, TSTypeParameterInstantiation } from "jscodeshift";
+import type { API, FileInfo, TaggedTemplateExpression, TSTypeLiteral, TSTypeParameterInstantiation } from "jscodeshift";
 import { defaultJSXAttributes } from "./constants.js";
 
 export default function transform(file: FileInfo, api: API) {
@@ -6,85 +6,26 @@ export default function transform(file: FileInfo, api: API) {
   const root = j(file.source);
 
   root.find(j.VariableDeclarator).forEach((path) => {
-    let isDirty = false;
+    if (path.node.init?.type !== "TaggedTemplateExpression") {
+      return;
+    }
 
-    if (path.node.init?.type === "TaggedTemplateExpression") {
-      if (
-        path.node.init.tag.type === "MemberExpression" &&
-        "typeParameters" in path.node.init &&
-        path.node.init.tag.object.type === "Identifier" &&
-        // only styled-components eg. const Button = styled.button`...`
-        path.node.init.tag.object.name === "styled"
-      ) {
-        // edit eg. styled.button<{ isPrimary: boolean; color: string }>...  ===> styled.button<{ $isPrimary: boolean; $color: string }>...
-        if ((path.node.init.typeParameters as TSTypeParameterInstantiation).type === "TSTypeParameterInstantiation") {
-          const typeParameters = path.node.init.typeParameters as TSTypeParameterInstantiation;
-          (typeParameters.params[0] as TSTypeLiteral).members.forEach((member) => {
-            if (member.type === "TSPropertySignature" && member.key.type === "Identifier") {
-              const propNameValue = member.key.name;
+    if (
+      path.node.init.tag.type === "MemberExpression" &&
+      "typeParameters" in path.node.init &&
+      path.node.init.tag.object.type === "Identifier" &&
+      // only styled-components eg. const Button = styled.button`...`
+      path.node.init.tag.object.name === "styled"
+    ) {
+      // edit eg. styled.button<{ isPrimary: boolean; color: string }>... => styled.button<{ $isPrimary: boolean; $color: string }>...
+      const isDirty = transformStyledComponentsGeneric(path.node.init);
 
-              if (!propNameValue.startsWith("$")) {
-                member.key.name = `$${propNameValue}`;
-                isDirty = true;
-              }
-            }
-          });
-        }
-
-        if (isDirty && path.node.init.quasi.type === "TemplateLiteral" && path.node.init.quasi.expressions.length > 0) {
-          path.node.init.quasi.expressions.forEach((expression) => {
-            if (expression.type !== "ArrowFunctionExpression") {
-              return;
-            }
-
-            const expressionParam = expression.params[0];
-
-            if (
-              expressionParam?.type === "ObjectPattern" &&
-              expressionParam.properties[0]?.type === "ObjectProperty" &&
-              expressionParam.properties[0].value &&
-              expressionParam.properties[0].value.type === "Identifier"
-            ) {
-              const objParam = expressionParam.properties[0].value.name;
-
-              if (!objParam.startsWith("$")) {
-                expressionParam.properties[0].value.name = `$${objParam}`;
-              }
-            }
-
-            if (expression.body.type === "MemberExpression" && expression.body.property.type === "Identifier") {
-              const propNameValue = expression.body.property.name;
-
-              if (!propNameValue.startsWith("$")) {
-                expression.body.property.name = `$${propNameValue}`;
-              }
-            } else if (expression.body.type === "ConditionalExpression") {
-              if (expression.body.test.type === "MemberExpression" && expression.body.test.property.type === "Identifier") {
-                const propNameValue = expression.body.test.property.name;
-
-                if (!propNameValue.startsWith("$")) {
-                  expression.body.test.property.name = `$${propNameValue}`;
-                }
-              } else if (expression.body.test.type === "BinaryExpression" && expression.body.test.left.type === "Identifier") {
-                const conditionValue = expression.body.test.left.name;
-
-                if (!conditionValue.startsWith("$")) {
-                  expression.body.test.left.name = `$${conditionValue}`;
-                }
-              }
-            } else if (
-              expression.body.type === "LogicalExpression" &&
-              expression.body.left.type === "MemberExpression" &&
-              expression.body.left.property.type === "Identifier"
-            ) {
-              const propNameValue = expression.body.left.property.name;
-
-              if (!propNameValue.startsWith("$")) {
-                expression.body.left.property.name = `$${propNameValue}`;
-              }
-            }
-          });
-        }
+      /**
+       * edit eg.
+       * width: ${(props) => (props.size === "s" ? "100px" : "200px")}... => width: ${(props) => (props.$size === "s" ? "100px" : "200px")}
+       */
+      if (isDirty) {
+        transformTemplateLiteralExpressions(path.node.init);
       }
     }
   });
@@ -109,4 +50,81 @@ export default function transform(file: FileInfo, api: API) {
   });
 
   return root.toSource();
+}
+
+function transformStyledComponentsGeneric(init: TaggedTemplateExpression & Record<"typeParameters", unknown>) {
+  let isDirty = false;
+
+  if ((init.typeParameters as TSTypeParameterInstantiation).type === "TSTypeParameterInstantiation") {
+    const typeParameters = init.typeParameters as TSTypeParameterInstantiation;
+    (typeParameters.params[0] as TSTypeLiteral).members.forEach((member) => {
+      if (member.type === "TSPropertySignature" && member.key.type === "Identifier") {
+        const propNameValue = member.key.name;
+
+        if (!propNameValue.startsWith("$")) {
+          member.key.name = `$${propNameValue}`;
+          isDirty = true;
+        }
+      }
+    });
+  }
+
+  return isDirty;
+}
+
+function transformTemplateLiteralExpressions(init: TaggedTemplateExpression & Record<"typeParameters", unknown>) {
+  if (init.quasi.type === "TemplateLiteral" && init.quasi.expressions.length > 0) {
+    init.quasi.expressions.forEach((expression) => {
+      if (expression.type !== "ArrowFunctionExpression") {
+        return;
+      }
+
+      const expressionParam = expression.params[0];
+
+      if (
+        expressionParam?.type === "ObjectPattern" &&
+        expressionParam.properties[0]?.type === "ObjectProperty" &&
+        expressionParam.properties[0].value &&
+        expressionParam.properties[0].value.type === "Identifier"
+      ) {
+        const objParam = expressionParam.properties[0].value.name;
+
+        if (!objParam.startsWith("$")) {
+          expressionParam.properties[0].value.name = `$${objParam}`;
+        }
+      }
+
+      if (expression.body.type === "MemberExpression" && expression.body.property.type === "Identifier") {
+        const propNameValue = expression.body.property.name;
+
+        if (!propNameValue.startsWith("$")) {
+          expression.body.property.name = `$${propNameValue}`;
+        }
+      } else if (expression.body.type === "ConditionalExpression") {
+        if (expression.body.test.type === "MemberExpression" && expression.body.test.property.type === "Identifier") {
+          const propNameValue = expression.body.test.property.name;
+
+          if (!propNameValue.startsWith("$")) {
+            expression.body.test.property.name = `$${propNameValue}`;
+          }
+        } else if (expression.body.test.type === "BinaryExpression" && expression.body.test.left.type === "Identifier") {
+          const conditionValue = expression.body.test.left.name;
+
+          if (!conditionValue.startsWith("$")) {
+            expression.body.test.left.name = `$${conditionValue}`;
+          }
+        }
+      } else if (
+        expression.body.type === "LogicalExpression" &&
+        expression.body.left.type === "MemberExpression" &&
+        expression.body.left.property.type === "Identifier"
+      ) {
+        const propNameValue = expression.body.left.property.name;
+
+        if (!propNameValue.startsWith("$")) {
+          expression.body.left.property.name = `$${propNameValue}`;
+        }
+      }
+    });
+  }
 }
